d36d6e9d33010584b6101c43fcc84b367cc85faa
Erik Smistad <smistad@idi.ntnu.no>
2014-02-11 12:06:21 +0100

    updated USE file

--- CMake/Level-Set-SegmentationUse.cmake
+++ CMake/Level-Set-SegmentationUse.cmake
@@ -3,24 +3,30 @@
 ###########################################################
 
 #------------------------------------------------------------------------------
+# Options
+#------------------------------------------------------------------------------
+option (LS_USE_EXTRNAL_OUL "Use external OpenCLUtilityLibrary" OFF)
+
+#------------------------------------------------------------------------------
 # External libraries
 #------------------------------------------------------------------------------
-
-# GTK
-#find_package (PkgConfig REQUIRED)
-#pkg_check_modules (GTK2 REQUIRED gtk+-2.0 gthread-2.0)
 
 # SIPL
 find_package(SIPL PATHS "${Level-Set-Segmentation_BINARY_DIR}/SIPL" REQUIRED)
 include(${SIPL_USE_FILE})
 
-# OpenCLUtilities
-find_package(OCL-Utilities PATHS "${Level-Set-Segmentation_BINARY_DIR}/OpenCLUtilities" REQUIRED)
-include(${OCL-Utilities_USE_FILE})
+# OpenCLUtilityLibrary
+if(LS_USE_EXTRNAL_OUL)
+    message(STATUS "Using external use file for OpenCLUtilityLibrary in LS: "${TSF_EXTERNAL_OUL_USEFILE})
+    include(${LS_EXTERNAL_OUL_USEFILE})
+else(LS_USE_EXTRNAL_OUL)
+    message(STATUS "Using submodule for OpenCLUtility in LS")
+    find_package(OpenCLUtilityLibrary PATHS "${Level-Set-Segmentation_BINARY_DIR}/OpenCLUtilityLibrary" REQUIRED)
+    include(${OpenCLUtilityLibrary_USE_FILE})
+endif(LS_USE_EXTRNAL_OUL)
 
 #------------------------------------------------------------------------------
 # Where to look for includes and libraries
 #------------------------------------------------------------------------------
 include_directories( ${Level-Set-Segmentation_INCLUDE_DIRS}  ${Level-Set-Segmentation_BINARY_DIR})
 link_directories (${Level-Set-Segmentation_LIBRARY_DIRS})
-
