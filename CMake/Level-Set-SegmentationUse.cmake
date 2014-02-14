e088efa2740ed7fa1018361b850c3c1cc26040cb
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2014-02-14 13:33:46 +0100

    Refactored CMakeLists to accept external OpenCLUtilityLibrary. Also had to remove directory scoped includes to prevent including submodule files when using external oul.

--- CMake/Level-Set-SegmentationUse.cmake
+++ CMake/Level-Set-SegmentationUse.cmake
@@ -3,29 +3,6 @@
 ###########################################################
 
 #------------------------------------------------------------------------------
-# Options
-#------------------------------------------------------------------------------
-option (LS_USE_EXTRNAL_OUL "Use external OpenCLUtilityLibrary" OFF)
-
-#------------------------------------------------------------------------------
-# External libraries
-#------------------------------------------------------------------------------
-
-# SIPL
-find_package(SIPL PATHS "${Level-Set-Segmentation_BINARY_DIR}/SIPL" REQUIRED)
-include(${SIPL_USE_FILE})
-
-# OpenCLUtilityLibrary
-if(LS_USE_EXTRNAL_OUL)
-    message(STATUS "Using external use file for OpenCLUtilityLibrary in LS: "${TSF_EXTERNAL_OUL_USEFILE})
-    include(${LS_EXTERNAL_OUL_USEFILE})
-else(LS_USE_EXTRNAL_OUL)
-    message(STATUS "Using submodule for OpenCLUtility in LS")
-    find_package(OpenCLUtilityLibrary PATHS "${Level-Set-Segmentation_BINARY_DIR}/OpenCLUtilityLibrary" REQUIRED)
-    include(${OpenCLUtilityLibrary_USE_FILE})
-endif(LS_USE_EXTRNAL_OUL)
-
-#------------------------------------------------------------------------------
 # Where to look for includes and libraries
 #------------------------------------------------------------------------------
 include_directories( ${Level-Set-Segmentation_INCLUDE_DIRS}  ${Level-Set-Segmentation_BINARY_DIR})
